988b22c397fef8a52b775738017db4ce1fedd1f4
4real890 <handsdesouth@gmail.com>
2025-06-14 05:21:29 +0300

    Update Configurations.js

--- Configurations.js
+++ Configurations.js
@@ -2,13 +2,13 @@ require("dotenv").config();
 
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "918101187835";   // You can replace this number with yours //
+  gg = "255785314172";   // You can replace this number with yours //
 }
 
 
 global.owner = gg.split(",");
-global.mongodb = process.env.MONGODB || "Put your alternative mongodb url here";
-global.sessionId = process.env.SESSION_ID || "ok";
+global.mongodb = process.env.MONGODB || "mongodb+srv://Qutaybah:Abubakari12345@cluster0.fn2kdat.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+global.sessionId = process.env.SESSION_ID || "Abubakari12345";
 global.prefa = process.env.PREFIX || "-";
 global.tenorApiKey = process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
 global.packname = process.env.PACKNAME || `Atlas Bot`;
