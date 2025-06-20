6bef49eb8283e315516c181a969f9570c501d676
4real890 <handsdesouth@gmail.com>
2025-06-20 15:10:11 +0300

    Update Configurations.js

--- Configurations.js
+++ Configurations.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "255785314172";   // You can replace this number with yours //
+  gg = "255624314178";   // You can replace this number with yours //
 }
 
 
